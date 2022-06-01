11ca367240420aeb1cc8a5d1baca392b13b3810b
Helgeee <olegvotoz@gmail.com>
2022-06-02 00:34:37 +0300

    v1

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Zadanie_1
+{
+    class Program
+    {
+        private static void Main()
+        {
+            Console.WriteLine("Введите X и Y");
+            int x = Convert.ToInt32(Console.ReadLine());
+            int y = Convert.ToInt32(Console.ReadLine());
+           
+            int z = max( x, 2 * y - x) + max(5 * x + 3 * y, y);
+            Console.WriteLine(z);
+        }
+
+        private static int max(int p0, int p1)
+        {
+            return p0 > p1 ? p0 : p1;
+        }
+    }
+}
